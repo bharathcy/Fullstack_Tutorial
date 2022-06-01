7f34df001a2c21b16762348e753cd4d8d1480c81
bharathcy <bharathcy@gmail.com>
2022-06-01 15:44:59 +0530

    updated coding js

--- js/Coding_Interviews/Coding.js
+++ js/Coding_Interviews/Coding.js
@@ -4,7 +4,39 @@
 let inp = "Bharath CY"
 //console.log(inp.split(''))
 //console.log([...inp])
-	
+
+/*--------------------------------------------------------------------------------
+									Check the ouput
+--------------------------------------------------------------------------------*/
+let abc
+let ghi = new Array(3)
+let jkl = {}
+let lmn = ""
+let opq = 0
+let rst = false
+let def = null				//***********IMP************
+
+
+// console.log(abc, def)
+// console.log(abc == def)
+// console.log("type of abc => ", typeof(abc))
+// console.log("type of def => ", typeof(def))
+// console.log("type of ghi => ", typeof(ghi))
+// console.log("type of jkl => ", typeof(jkl))
+// console.log("type of lmn => ", typeof(lmn))
+// console.log("type of opq => ", typeof(opq))
+// console.log("type of rst => ", typeof(rst))
+
+
+
+/*--------------------------------------------------------------------------------
+									Remove Duplicates
+--------------------------------------------------------------------------------*/
+const removeDuplicate = []
+array_name = [1, 3, 5, 77, 5, 77, 86, 3, 90]
+output_array = array_name.filter((element, index) => array_name.indexOf(element) == index);
+//console.log(output_array)
+
 
 /*--------------------------------------------------------------------------------
 									Reverse a string in JS
@@ -66,6 +98,49 @@ Example_Output = anagram(['kmno', 'okmn', 'omkn', 'dell', 'ledl', 'elld', 'ko'])
 //console.log(Example_Output)
 
 
+
+
+
+
+
+
+/*--------------------------------------------------------------------------------
+								Armstrong 3 digit Number
+--------------------------------------------------------------------------------*/
+
+function Armstrong(original_number){
+	let sum = 0;
+	let temporary_number = original_number;
+	while (temporary_number > 0) {
+		//Fetch Last Digit
+	    let remainder = temporary_number % 10;
+
+
+		const const_remainder = remainder
+	    for (let i=1; i<original_number.toString().length;i++){
+	    	//multiply the number on its length times
+	    	remainder = const_remainder * remainder
+	    }
+	
+	    //add the multiplied numbers
+	    sum += remainder;
+	    
+
+	    //Remove The Last Digit
+	    temporary_number = parseInt(temporary_number / 10);	
+	
+	}
+
+	if (sum == original_number)
+	{
+	    console.log(`${original_number} is an Armstrong number`);
+	}
+	else
+	{
+	    console.log(`${original_number} is not an Armstrong number.`);
+	}
+}
+Armstrong(896)
 /*--------------------------------------------------------------------------------
 									Chess board Layout
 --------------------------------------------------------------------------------*/
